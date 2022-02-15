da0fc28e22009ef8c54295f33e84a12762c2c3b7
mydemo0902 <98962940+mydemo0902@users.noreply.github.com>
2022-02-15 09:18:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,5 +18,7 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0a8b4cd432b1c3063"
   instance_type = "t2.micro"
+  security_groups = [ "My VPC Security Group" ]
+  key_name = "myaws"
 }
 
