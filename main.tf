99fbff1f529a71252e0e8edf1d88e90dcdffc2ce
mydemo0902 <98962940+mydemo0902@users.noreply.github.com>
2022-02-15 09:47:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
 provider "aws" {
   
   region  = "us-east-1"
